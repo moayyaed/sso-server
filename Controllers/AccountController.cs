597ad61f4309fe9187e8bb1706ad829d83b510fc
duycs <duycs@outlook.com>
2019-12-31 23:47:54 +0700

    add TODO: Error Field 'Id' doesn't have a default value

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -166,11 +166,16 @@ namespace AuthServer.Controllers
             var result = await _userManager.CreateAsync(user, model.Password);
 
             if (!result.Succeeded) return BadRequest(result.Errors);
-            
-            await _userManager.AddClaimAsync(user, new System.Security.Claims.Claim("userName", user.UserName));
-            await _userManager.AddClaimAsync(user, new System.Security.Claims.Claim("name", user.Name));
-            await _userManager.AddClaimAsync(user, new System.Security.Claims.Claim("email", user.Email));
-            await _userManager.AddClaimAsync(user, new System.Security.Claims.Claim("role", Roles.Consumer));
+
+            try
+            {
+                //TODO: Error Field 'Id' doesn't have a default value 
+                await _userManager.AddClaimAsync(user, new System.Security.Claims.Claim("userName", user.UserName));
+                await _userManager.AddClaimAsync(user, new System.Security.Claims.Claim("name", user.Name));
+                await _userManager.AddClaimAsync(user, new System.Security.Claims.Claim("email", user.Email));
+                await _userManager.AddClaimAsync(user, new System.Security.Claims.Claim("role", Roles.Consumer));
+            }
+            catch (Exception ex) { }
 
             return Ok(new RegisterResponseViewModel(user));
         }
